4858449de7693bf6027cb0e2165c4a42ba9f23cb
Kristýna Adlofová <kristyna.adlofova@gmail.com>
2023-07-20 01:13:29 +0200

    SCSS-colors,fonts+breakpoints

--- 404.php
+++
@@ -1,6 +0,0 @@
-<?php get_header(); ?>
-    <div class="error">
-    <h1>Ups, tato stránka neexistuje.</h1> 
-    <a href="https://kavarna1praha.eu/">Zpět na Prima Kavárna</a>
-    </div>
-<?php get_footer(); ?>
